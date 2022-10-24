f48ca319b9403f84423b96f69269efa5fa206cc2
magocod <magoyeison@gmail.com>
2022-10-23 20:49:24 -0400

    update ci

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,47 +1,47 @@
 import {
   createDinero,
-  assert,
-  INVALID_AMOUNT_MESSAGE,
-  INVALID_SCALE_MESSAGE,
+  // assert,
+  // INVALID_AMOUNT_MESSAGE,
+  // INVALID_SCALE_MESSAGE,
 } from "@dinero.js/core";
 
-import {
-  add,
-  compare,
-  decrement,
-  increment,
-  integerDivide,
-  modulo,
-  multiply,
-  power,
-  subtract,
-  toNumber,
-  zero,
-} from "@crates/calculator-napi";
+// import {
+//   add,
+//   compare,
+//   decrement,
+//   increment,
+//   integerDivide,
+//   modulo,
+//   multiply,
+//   power,
+//   subtract,
+//   toNumber,
+//   zero,
+// } from "@crates/calculator-napi";
 
 import * as dinBigInt from "@dinero.js/calculator-bigint";
 
-const calculatorNapi = {
-  add,
-  compare,
-  decrement,
-  increment,
-  integerDivide,
-  modulo,
-  multiply,
-  power,
-  subtract,
-  toNumber,
-  zero,
-};
+// const calculatorNapi = {
+//   add,
+//   compare,
+//   decrement,
+//   increment,
+//   integerDivide,
+//   modulo,
+//   multiply,
+//   power,
+//   subtract,
+//   toNumber,
+//   zero,
+// };
 
-export const dineroNapi = createDinero({
-  calculator: calculatorNapi,
-  onCreate({ amount, scale }) {
-    assert(Number.isInteger(amount), INVALID_AMOUNT_MESSAGE);
-    assert(Number.isInteger(scale), INVALID_SCALE_MESSAGE);
-  },
-});
+// export const dineroNapi = createDinero({
+//   calculator: calculatorNapi,
+//   onCreate({ amount, scale }) {
+//     assert(Number.isInteger(amount), INVALID_AMOUNT_MESSAGE);
+//     assert(Number.isInteger(scale), INVALID_SCALE_MESSAGE);
+//   },
+// });
 
 export const dineroBigint = createDinero({
   calculator: dinBigInt.calculator,
